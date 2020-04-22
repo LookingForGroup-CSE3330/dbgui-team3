1f9efbe511c6680695b002d844ed6b5c322aeaf6
DylanFurner-71 <jdfurner@gmail.com>
2020-04-22 01:23:12 -0500

    Version 0.0.0.1

--- backend/node/routes/users.js
+++ backend/node/routes/users.js
@@ -6,21 +6,21 @@ router.get('/users/get', (req, res) => {
     connection.query('select * from db.users', (err, result, fields) => {
         if(err) throw err
         res.send(JSON.stringify(result))
-    });
-});
+    })
+})
 
 router.get('/users/get/:username', (req, res) => {
-    connection.query('select * from db.users where username = ', (err, result, fields) => {
+    connection.query('select * from db.users where db.users.username = ${req.params.uername', (err, result, fields) => {
         if(err) throw err
         res.send(JSON.stringify(result)) 
-    });
-});
+    })
+})
 router.get('/users/viewaccount/:username', (req, res) => {
        connection.query('select * from db.users where db.users.username = ', (err, result, fields) => {
         if(err) throw err
         res.send(JSON.stringify(result)) 
-    });
-});
+    })
+})
 //create an account
 router.post('/users/createaccount/', (req, res) => {
   var sqlstmnt = `INSERT INTO users(username, password_p, about_me, up_votes, down_votes) VALUES (\'${req.query.password}\', \'${req.query.username}\)`
@@ -28,8 +28,8 @@ router.post('/users/createaccount/', (req, res) => {
     connection.query(sqlstmnt, function (err, result, fields) {
         if (err) throw err;
         res.end(JSON.stringify(result)); // Result in JSON format
-    });
-});
+    })
+})
 //Delete a user
 router.delete('/users/:username/delete', async (req, res) => {
   let sql = `DELETE FROM users WHERE username = ${req.params.username}`;
@@ -38,7 +38,7 @@ router.delete('/users/:username/delete', async (req, res) => {
         if (err)
             return console.error(error.message);
         res.end(JSON.stringify(result));
-      });
-});
+      })
+})
 
 module.exports = router
