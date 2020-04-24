498ec666652535b76c3baa9b99eb17adca438a48
Tim Lee <hunjael@smu.edu>
2020-04-23 20:16:02 -0700

    user signup with password hashing

--- backend/node/routes/users.js
+++ backend/node/routes/users.js
@@ -1,6 +1,7 @@
 //example file to route
 const router = require('express').Router()
 const connection = require('../server')
+const bcrypt = require('bcrypt')
 
 router.get('/users/get', (req, res) => {
     connection.query('select * from db.users', (err, result, fields) => {
@@ -20,20 +21,36 @@ router.get('/users/viewaccount/:username', (req, res) => {
        connection.query('select * from db.users where db.users.username =  ${req.params.username}', (err, result, fields) => {
         if(err) throw err
         res.send(JSON.stringify(result))   
-    })  
+    })       
 })
-//create an account
-router.post('/users/createaccount/', (req, res) => {
-   var account = {
-        username: req.body.username,
-        password_p: req.body.user_id,
-        about_me: req.body.about_me,
-    }
-    connection.query('insert into db.users SET ?', account, (err, result, fields) => {
+
+//USER Authentication   
+
+//Signup (includes password hashing so we dont store plaintext passwords in the db)
+router.post('/users/signup/', (req, res) => {
+    bcrypt.hash(req.body.password_p, 0, (err, hash) => {
         if(err) throw err
-        res.send(JSON.stringify(result))
+        else {
+            var account = {
+                username: req.body.username,
+                password_p: hash,
+                about_me: req.body.about_me,
+                profile_img: req.body.profile_img,
+                email: req.body.email,
+                credentials: req.body.credentials
+            }
+            connection.query('insert into db.users SET ?', account, (err, result, fields) => {
+                if(err) throw err
+                res.send(JSON.stringify(result))
+            })
+        } //end else
     })
 })
+
+//Login: with JWT auth
+
+
+
 //Delete a user
 router.delete('/users/:username/delete', async (req, res) => {
   let sql = `DELETE FROM db.users WHERE db.username = ${req.params.username}`;
