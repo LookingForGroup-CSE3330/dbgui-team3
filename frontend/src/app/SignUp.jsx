663ffc9acc3fc7c0b90a94fe3b639de96d1bd1d2
wildermason <wildermason20@gmail.com>
2020-04-25 12:51:20 -0500

    changing page to fit db fields

--- frontend/src/app/SignUp.jsx
+++ frontend/src/app/SignUp.jsx
@@ -4,15 +4,11 @@ export class SignUp extends React.Component{
     state = {
         username: '',
         password_p: '',
-        firstName: '',
-        lastName: '',
-        age: '',
-        gender: '',
-        isDoctor: 0,
         email: '',
-        phoneNumber: '',
-        about_me: ''
+        about_me: '',
+        credentials: ''
     }
+
 
     render(){
         return(
@@ -26,34 +22,6 @@ export class SignUp extends React.Component{
                         <form>
                             <div className="form-group row">
                                 <div className="col">
-                                    <label htmlFor="fNameInput">First Name</label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        id="fNameInput"
-                                        name="fNameInput"
-                                        placeholder="First name"
-                                        value={this.state.firstName}
-                                        onChange={e => this.setState({ firstName: e.target.value})}
-                                        required
-                                    />
-                                </div>
-                                <div className="col">
-                                    <label htmlFor="lNameInput">Last name</label>
-                                    <input 
-                                        type="text"
-                                        className="form-control"
-                                        id="lNameInput"
-                                        name="lNameInput"
-                                        placeholder="last name"
-                                        value={this.state.lastName}
-                                        onChange={e => this.setState({ lastName: e.target.value})}
-                                        required
-                                    />
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <div className="col">
                                     <label htmlFor="userNameInput" >Username</label>
                                     <input 
                                         type="text"
@@ -61,8 +29,8 @@ export class SignUp extends React.Component{
                                         id="userNameInput"
                                         name="userNameInput"
                                         placeholder="Username"
-                                        value={this.state.userName}
-                                        onChange={e => this.setState({userName: e.target.value})}   
+                                        value={this.state.username}
+                                        onChange={e => this.setState({username: e.target.value})}   
                                         required
                                     />
                                 </div>
@@ -74,78 +42,50 @@ export class SignUp extends React.Component{
                                         id="passwordInput"
                                         name="passwordInput"
                                         placeholder="Password"
-                                        value={this.state.password}
-                                        onChange={e => this.setState({password: e.target.value})}
+                                        value={this.state.password_p}
+                                        onChange={e => this.setState({password_p: e.target.value})}
                                         required
                                     />
-                                </div>
-                            </div>
-                            <div className="form-group row">
-                                <div className="col">
-                                    <label htmlFor="emailInput">Email</label>
-                                    <input 
-                                        type="email"
-                                        className="form-control"
-                                        id="emailInput"
-                                        name="emailInput"
-                                        placeholder="Email"
-                                        value={this.state.email}
-                                        onChange={e => this.setState({email: e.target.value})}
-                                        required
-                                    />
-                                </div>
-                                <div className="col">
-                                    <label htmlFor="phoneInput">Phone</label>
-                                    <input 
-                                        type="tel"
-                                        className="form-control"
-                                        id="phoneInput"
-                                        name="phoneInput"
-                                        pattern="[0-9]{10}"
-                                        value={this.state.phoneNumber}
-                                        onChange={e => this.setState({phoneNumber: e.target.value})}
-                                        required
-                                    />
-                                </div>   
-                            </div>
-                            <div className="form-group row">
-                                <div className="col">
-                                    <label htmlFor="ageInput">Age</label>
-                                    <input 
-                                        type="date"
-                                        className="form-control"
-                                        id="ageInput"
-                                        name="ageInput"
-                                        value={this.state.age}
-                                        onChange={e => this.setState({age: e.target.value})}
-                                        required
-                                    />
-                                </div>
-                                <div className="col">
-                                    <label htmlFor="genderInput">Gender</label>
-                                    <select 
-                                        name="genderInput" 
-                                        id="genderInput"
-                                        className="form-control"
-                                        required
-                                    >
-                                        <option selected>Choose...</option>
-                                        <option value="Male">Male</option>
-                                        <option value="Female">Female</option>
-                                    </select>
                                 </div>
                             </div>
                             <div className="form-group">
+                                <label htmlFor="emailInput">Email</label>
+                                <input 
+                                    type="email"
+                                    className="form-control"
+                                    id="emailInput"
+                                    name="emailInput"
+                                    placeholder="someone@example.com"
+                                    value={this.state.email}
+                                    onChange={e => this.setState({email: e.target.value})}
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="aboutInput">About Me</label>
+                                <textarea 
+                                    name="aboutInput" id="aboutInput" className="form-control"></textarea>
+                            </div>
+                            <div className="form-group">
                                     <div className="form-check">
+                                        <label htmlFor="isDoctor" className="form-check-label">Are you a doctor? </label>&nbsp;
                                         <input 
                                             type="checkbox"
                                             id="isDoctor"
                                             name="isDoctor"
-                                            className="form-check-input"
-                                            value={this.state.isDoctor}
-                                            onChange={e => this.setState({age: e.target.value})}
                                         />
-                                        <label htmlFor="isDoctor" className="form-check-label">Are you a doctor?</label>
+                                        
+                                    </div>
+                                    <div className="col-md" id="credBox" style={{display: 'none'}}>
+                                        <label htmlFor="credInput"></label>
+                                        <input 
+                                            type="text"
+                                            name="credInput"
+                                            id="credInput"
+                                            placeholder="M.D. University of School"
+                                            value={this.state.credentials}
+                                            onChange={e => this.setState({credentials: e.target.value})}
+                                        />
                                     </div>
                                 </div>
                             <hr/>
